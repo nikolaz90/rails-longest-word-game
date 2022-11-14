48f77ae4ed773b6fc689168033f015546d379d3d
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2022-11-14 17:57:52 +0100

    added the word has to be in grid rule

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,11 +8,23 @@ class GamesController < ApplicationController
 
   def score
     @answer = params[:users_word]
-    @letters = params[:letters].to_a
+    @letters = params[:letters].gsub!(" ", "").upcase
     url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
     fetch = URI.open(url).read
     data = JSON.parse(fetch)
-    @response = 'Hmmm... thats not in the dictionary...'
-    @response = 'Well done' if data['found']
+    if attempt_in_grid?(@answer, @letters)
+      @response = 'Hmmm... thats not in the dictionary...'
+      @response = 'Well done' if data['found']
+    else
+      @response = 'that has letters not in the grid...'
+    end
+  end
+
+  def attempt_in_grid?(attempt, grid)
+    attempt_is_in_grid = true
+    attempt.chars.each do |i|
+      attempt_is_in_grid = false unless grid.include?(i.upcase)
+    end
+    attempt_is_in_grid
   end
 end
