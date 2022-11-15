c6ab4585748844571d010a928d28fa1a51f3ea16
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2022-11-15 15:03:38 +0100

    refactored controller slightly and added some game rules

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,28 +3,35 @@ require 'json'
 
 class GamesController < ApplicationController
   def new
-    @letters = ('a'..'z').to_a.sample(10)
+    @letters = Array.new(10) { ('a'..'z').to_a.sample }
   end
 
   def score
-    @answer = params[:users_word]
+    @attempt = params[:users_word].upcase
     @letters = params[:letters].gsub!(" ", "").upcase
-    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
     fetch = URI.open(url).read
     data = JSON.parse(fetch)
-    if attempt_in_grid?(@answer, @letters)
-      @response = 'Hmmm... thats not in the dictionary...'
-      @response = 'Well done' if data['found']
-    else
-      @response = 'that has letters not in the grid...'
-    end
+    define_response(@attempt, @letters, data)
+  end
+
+  def define_response(attempt, grid, data)
+    @response = 'that has letters not in the grid...'
+    @response = 'Hmmm... thats not in the dictionary...' if attempt_in_grid?(attempt, grid)
+    @response = 'Well done' if data['found']
+    @response = 'You have used to many letters' if over_used?(@attempt, @letters)
   end
 
   def attempt_in_grid?(attempt, grid)
     attempt_is_in_grid = true
     attempt.chars.each do |i|
-      attempt_is_in_grid = false unless grid.include?(i.upcase)
+      attempt_is_in_grid = false unless grid.include?(i)
     end
     attempt_is_in_grid
+  end
+
+  def over_used?(attempt, grid)
+    attempt.chars.each { |item| return true if grid.count(item) < attempt.count(item) }
+    false
   end
 end
